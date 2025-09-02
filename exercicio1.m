a0c9b1271d465dbf96e5ef7904ae2c2531cc81ee
Bruno Bellini <69762038+ChunoB@users.noreply.github.com>
2025-09-02 10:57:46 -0300

    exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,33 +1,38 @@
-function t = exercicio1(func,func_d,x0)
+function t = exercicio1(func, func_d, x0)
 
-% nao alterar: inicio
-es = 0.01;
-imax = 20;
-% nao alterar: fim
+  % nao alterar: inicio
+  es = 0.01;
+  imax = 20;
+  % nao alterar: fim
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Valores iniciais para a secante
+  x_prev = x0 * 0.9;  % pequeno ajuste
+  x_curr = x0;
 
-v = zeros(imax,1);   
-v(1) = x0;            
-erro = zeros(imax,1); 
+  for ii = 1:imax
+    f_prev = func(x_prev);
+    f_curr = func(x_curr);
 
-printf("Iteração | Aproximação     | Erro Relativo\n");
-printf("-------------------------------------------\n");
-
-for ii = 1:imax-1
-  if ii ~= 1
-    erro(ii) = abs((v(ii)-v(ii-1))/v(ii));
-    printf("%3d      | %12.6f   | %12.6f\n", ii, v(ii), erro(ii));
-    if erro(ii) < es
-      break;
+    % Evita divisão por zero
+    if f_curr == f_prev
+      break
     endif
-  else
-    printf("%3d      | %12.6f   |    -----------\n", ii, v(ii));
-  endif
-  v(ii+1) = v(ii) - func(v(ii))/func_d(v(ii));
-endfor
 
-t = v(ii);   % último valor é a raiz
+    % Passo da secante
+    x_next = x_curr - f_curr * (x_curr - x_prev) / (f_curr - f_prev);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Checa convergência
+    if abs(x_next - x_curr) < es
+      t = x_next;
+      return
+    endif
+
+    % Atualiza para próxima iteração
+    x_prev = x_curr;
+    x_curr = x_next;
+  endfor
+
+  % Se não convergiu, retorna último valor
+  t = x_curr;
+
 endfunction
