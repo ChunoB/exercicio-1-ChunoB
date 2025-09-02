94b7850d86deb453ed752d3cac2629d4644fae79
ChunoB <bruno.b.bellini@gmail.com>
2025-09-02 13:48:29 -0300

    modificado

--- exercicio1.m
+++ exercicio1.m
@@ -1,15 +1,26 @@
-function t = exercicio1(func,x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 1;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% digite seu codigo aqui
-t = 0; % alterar
-
+t = 0;
+x = zeros(imax,1);
+x(1) = x0;
+erros = zeros(imax,1);
+for j = 1:imax -1
+  x(j+1) = x(j) - (func(x(j)) / func_d(x(j)));
+  t = x(j+1);
+    if j ~=1
+      erros(j) = abs((x(j) - x(j-1)) / x(j));
+      if erros(j) <= es
+        break
+      endif
+    endif
+endfor
+t
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
