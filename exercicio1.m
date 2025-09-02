3b129d4bb2424aba2d33650454a23eeb661f99de
Bruno Bellini <69762038+ChunoB@users.noreply.github.com>
2025-09-02 13:43:08 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,26 +1,15 @@
-function t = exercicio1(func,func_d,x0)
+function t = exercicio1(func,x0)
 
 % nao alterar: inicio
-es = 0.01;
+es = 1;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-t = 0;
-x = zeros(imax,1);
-x(1) = x0;
-erros = zeros(imax,1);
-for j = 1:imax -1
-  x(j+1) = x(j) - (func(x(j)) / func_d(x(j)));
-  t = x(j+1);
-    if j ~=1
-      erros(j) = abs((x(j) - x(j-1)) / x(j));
-      if erros(j) <= es
-        break
-      endif
-    endif
-endfor
-t
+
+% digite seu codigo aqui
+t = 0; % alterar
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
